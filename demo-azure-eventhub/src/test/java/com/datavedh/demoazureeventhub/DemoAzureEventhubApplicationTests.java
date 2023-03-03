3e6a3a5b8a0bf1a27835819d28d23cd1c0054a29
vinitamahajan <vinita.gavali@gmail.com>
2023-03-03 11:00:07 +0530

    EmitEvents Event Hub
    Initial Commit Hackathon DataVedh 2023

---
+++ demo-azure-eventhub/src/test/java/com/datavedh/demoazureeventhub/DemoAzureEventhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datavedh.demoazureeventhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAzureEventhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
